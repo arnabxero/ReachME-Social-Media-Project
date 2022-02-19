5252423fd561224789cb52d34f7b5320d9a3b845
Ehtimam <70962870+Ehtimam@users.noreply.github.com>
2022-02-20 01:30:34 +0600

    Update db.php

--- Source Code/db.php
+++ Source Code/db.php
@@ -3,21 +3,13 @@ class dbConfig
 {
     
     public $host;
-
-   
     public $username;
-
-    
     public $password;
-
-  
     public $dbName = NULL;
-
-   
-   public $conn;
+    public $conn;
 
     public function dbConnect() {
-        $this->conn = mysqli_connect($this->host,$this->username,$this->password,$this->dbName);
+        $this->conn = mysqli_connect($this->host, $this->username, $this->password, $this->dbName);
     
         if (!$this->conn) {
             die("Connection failed: " . mysqli_connect_error());
